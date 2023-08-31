2383d6b00659c7fd451c17c838c82e72ac674442
Morten Lohne <lohnemorten@gmail.com>
2023-09-01 00:51:17 +0200

    Add cli test, remove deprecated clap functions

--- src/cli.rs
+++ src/cli.rs
@@ -2,8 +2,8 @@ use crate::{
     openings::{self, BookFormat},
     uci::parser,
 };
-use clap::{self, App, Arg};
-use std::{env, ffi::OsString, num::NonZeroUsize, time::Duration};
+use clap::{self, Arg, Command};
+use std::{env, ffi::OsString, time::Duration};
 use tiltak::position::Komi;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -31,134 +31,118 @@ pub fn parse_cli_arguments() -> CliOptions {
 pub fn parse_cli_arguments_from(
     itr: impl Iterator<Item = OsString>,
 ) -> Result<CliOptions, clap::Error> {
-    let matches = App::new("Racetrack")
+    let matches = Command::new("Racetrack")
         .version("0.2.1")
         .author("Morten Lohne")
         .about("Play a match between two or more Tak engines")
         .arg(
-            Arg::with_name("size")
+            Arg::new("size")
                 .short('s')
                 .long("size")
                 .help("Board size.")
                 .takes_value(true)
                 .default_value("5")
-                .possible_values(&["4", "5", "6", "7", "8"]),
+                .value_parser(clap::value_parser!(u64).range(4..=8)),
         )
-        .arg(Arg::with_name("engine-path")
+        .arg(Arg::new("engine-path")
             .help("Specify the file path of an engine. Must be used twice to add both engines.")
             .short('e')
             .long("engine")
             .required(true)
-            .multiple(true)
             .takes_value(true)
             .min_values(2)
             .max_values(2)
         )
-        .arg(Arg::with_name("concurrency")
+        .arg(Arg::new("concurrency")
             .help("Number of games to run in parallel.")
             .default_value("1")
             .short('c')
             .long("concurrency")
             .value_name("n")
-            .validator(|input| {
-                match input.parse::<usize>() {
-                    Ok(num) => {
-                        if num == 0 || num > 1024 {
-                            Err("Must be between 1 and 1024".to_string())
-                        } else { Ok(()) }
-                    }
-                    Err(err) => {
-                        Err(err.to_string())
-                    }
-                }
-            }))
-        .arg(Arg::with_name("games")
+            .value_parser(clap::value_parser!(u64).range(1..=1024)))
+        .arg(Arg::new("games")
             .help("Number of games to play.")
             .short('g')
             .long("games")
             .required(true)
-            .takes_value(true))
-        .arg(Arg::with_name("file")
+            .takes_value(true)
+            .value_parser(clap::value_parser!(usize)))
+        .arg(Arg::new("file")
             .help("Output file for all game PTNs.\nIf the file already exists, new games will be appended.")
             .long("ptnout")
             .takes_value(true))
-        .arg(Arg::with_name("book")
+        .arg(Arg::new("book")
             .help("Start each game from an opening from the file. Each opening is played twice, with different colors. If there are more game pairs than openings, the openings will start to repeat. An opening file is included in the git repository.")
             .short('b')
             .long("book")
             .takes_value(true)
             .value_name("file.txt"))
-        .arg(Arg::with_name("book-format")
+        .arg(Arg::new("book-format")
             .long("--book-format")
             .help("Opening book format. The included books are in the default 'move-list' format.")
             .takes_value(true)
             .requires("book")
             .default_value("move-list")
-            .possible_values(&["move-list", "tps", "ptn"]))
-        .arg(Arg::with_name("book-start")
+            .value_parser(["move-list", "tps", "ptn"]))
+        .arg(Arg::new("book-start")
             .long("--book-start")
             .help("Start from the opening with the specified index. Starts at 1.")
             .takes_value(true)
             .requires("book")
             .conflicts_with("shuffle-book")
-            .validator(|input|
-                input.parse::<NonZeroUsize>()
-                    .map(|_| ())
-                    .map_err(|err| err.to_string())
-                )
-            )
-        .arg(Arg::with_name("shuffle-book")
+            .value_parser(clap::value_parser!(u64).range(1..)))
+        .arg(Arg::new("shuffle-book")
             .long("--shuffle-book")
             .help("Shuffle the provided opening book.")
             .takes_value(false)
             .requires("book"))
-        .arg(Arg::with_name("tc")
+        .arg(Arg::new("tc")
             .help("Time control for each game, in seconds. Format is time+increment, where the increment is optional.")
             .long("tc")
             .takes_value(true)
             .required(true))
-        .arg(Arg::with_name("engine1-args")
+        .arg(Arg::new("engine1-args")
             .help("Command-line argument string to pass to engine 1.")
             .long("engine1-args")
             .takes_value(true)
             .value_name("args")
             .allow_hyphen_values(true)
         )
-        .arg(Arg::with_name("engine2-args")
+        .arg(Arg::new("engine2-args")
             .help("Command-line argument string to pass to engine 2.")
             .long("engine2-args")
             .takes_value(true)
             .value_name("args")
             .allow_hyphen_values(true)
         )
-        .arg(Arg::with_name("log")
+        .arg(Arg::new("log")
             .short('l')
             .long("log")
             .value_name("racetrack.log")
             .help("Name of debug logfile. If not set, no debug log will be written.")
             .takes_value(true),
         )
-        .arg(Arg::with_name("komi")
+        .arg(Arg::new("komi")
             .long("komi")
             .help("Play with komi, if the engines support it.")
             .takes_value(true)
             .allow_hyphen_values(true)
             .default_value("0")
-            .validator(|input| {
-                input.parse::<Komi>().map(|_| ())
+            .value_parser(|input: &str| {
+                input.parse::<Komi>()
             }),
         )
-        .get_matches_from_safe(itr)?;
+        .try_get_matches_from(itr)?;
 
-    let (time, increment) =
-        parser::parse_tc(matches.value_of("tc").unwrap()).unwrap_or_else(|err| panic!("{}", err));
+    let (time, increment) = parser::parse_tc(matches.get_one::<String>("tc").unwrap())
+        .unwrap_or_else(|err| panic!("{}", err));
 
     let engine_path_matches = matches
-        .values_of("engine-path")
+        .get_many::<String>("engine-path")
         .unwrap()
         .collect::<Vec<_>>();
 
-    let book_format = match matches.value_of("book-format").unwrap() {
+    let book_format = match matches.get_one::<String>("book-format").unwrap().as_str() {
         "move-list" => BookFormat::MoveList,
         "tps" => BookFormat::Fen,
         "ptn" => BookFormat::Pgn,
@@ -166,9 +150,9 @@ pub fn parse_cli_arguments_from(
     };
 
     Ok(CliOptions {
-        size: matches.value_of("size").unwrap().parse().unwrap(),
-        concurrency: matches.value_of("concurrency").unwrap().parse().unwrap(),
-        games: matches.value_of("games").unwrap().parse().unwrap(),
+        size: *matches.get_one::<u64>("size").unwrap() as usize,
+        concurrency: *matches.get_one::<u64>("concurrency").unwrap() as usize,
+        games: *matches.get_one::<usize>("games").unwrap(),
         time,
         increment,
         engine_paths: [
@@ -176,20 +160,15 @@ pub fn parse_cli_arguments_from(
             engine_path_matches[1].to_string(),
         ],
         engine_args: [
-            matches.value_of("engine1-args").map(ToOwned::to_owned),
-            matches.value_of("engine2-args").map(ToOwned::to_owned),
+            matches.get_one("engine1-args").cloned(),
+            matches.get_one("engine2-args").cloned(),
         ],
-        pgnout: matches.value_of("file").map(|s| s.to_string()),
-        book_path: matches.value_of("book").map(|s| s.to_string()),
+        pgnout: matches.get_one("file").cloned(),
+        book_path: matches.get_one("book").cloned(),
         book_format,
-        shuffle_book: matches.is_present("shuffle-book"),
-        book_start_index: matches
-            .value_of("book-start")
-            .unwrap_or("1")
-            .parse::<usize>()
-            .unwrap()
-            - 1,
-        log_file_name: matches.value_of("log").map(|s| s.to_string()),
-        komi: matches.value_of("komi").unwrap().parse().unwrap(),
+        shuffle_book: matches.contains_id("shuffle-book"),
+        book_start_index: *matches.get_one::<u64>("book-start").unwrap_or(&1) as usize - 1,
+        log_file_name: matches.get_one::<String>("log").cloned(),
+        komi: *matches.get_one::<Komi>("komi").unwrap(),
     })
 }
--- src/tests/cli_tests.rs
+++ src/tests/cli_tests.rs
@@ -7,7 +7,7 @@ use crate::openings;
 
 #[test]
 fn cli_test() {
-    let input = "./racetrack -s 6 --games 2000 --tc 60+0.6 --concurrency 10 --book 6s_4ply_balanced_openings.txt --ptnout tako_vs_tiltak.ptn -l racetrack.log --engine tiltak taktician";
+    let input: &str = "./racetrack -s 6 --games 2000 --tc 60+0.6 --concurrency 10 --book 6s_4ply_balanced_openings.txt --ptnout tako_vs_tiltak.ptn -l racetrack.log --shuffle-book --engine tiltak taktician";
 
     // "tei -multi-cut -table-mem 512000000" is quoted in the real input, which becomes one element in the argument list
     let tail_input = ["--engine2-args", "tei -multi-cut -table-mem 512000000"];
@@ -33,10 +33,42 @@ fn cli_test() {
         pgnout: Some("tako_vs_tiltak.ptn".to_string()),
         book_path: Some("6s_4ply_balanced_openings.txt".to_string()),
         book_format: openings::BookFormat::MoveList,
-        shuffle_book: false,
+        shuffle_book: true,
         book_start_index: 0,
         log_file_name: Some("racetrack.log".to_string()),
         komi: Komi::default(),
+    };
+
+    if let Err(err) = &cli_options {
+        eprintln!("{err}")
+    }
+
+    assert_eq!(cli_options.unwrap(), expected)
+}
+
+#[test]
+fn shuffle_book_test() {
+    let input: &str =
+        "./racetrack -s 5 --games 100 --tc 60+0.6 --book openings.ptn --engine tiltak taktician --komi 2.5 --book-start 10 --book-format ptn";
+
+    let cli_options =
+        cli::parse_cli_arguments_from(input.split_whitespace().map(|word| word.into()));
+
+    let expected = cli::CliOptions {
+        size: 5,
+        concurrency: 1,
+        games: 100,
+        time: Duration::from_secs(60),
+        increment: Duration::from_millis(600),
+        engine_paths: ["tiltak".to_string(), "taktician".to_string()],
+        engine_args: [None, None],
+        pgnout: None,
+        book_path: Some("openings.ptn".to_string()),
+        book_format: openings::BookFormat::Pgn,
+        shuffle_book: false,
+        book_start_index: 9,
+        log_file_name: None,
+        komi: Komi::from_half_komi(5).unwrap(),
     };
 
     if let Err(err) = &cli_options {
