a90945f05554b2c36c54e33acec3d8eead24e74c
Morten Lohne <lohnemorten@gmail.com>
2023-12-29 17:24:31 +0100

    Switch to using '--engine' and '--all-engines' positional arguments to configure 'n' different engines. Only exactly two is supported still

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,7 @@ use crate::{
     openings::{self, BookFormat},
     uci::parser,
 };
-use clap::{self, Arg, Command};
+use clap::{self, Arg, ArgAction, Command};
 use std::{env, ffi::OsString, time::Duration};
 use tiltak::position::Komi;
 
@@ -22,6 +22,15 @@ pub struct CliOptions {
     pub book_start_index: usize,
     pub log_file_name: Option<String>,
     pub komi: Komi,
+}
+
+#[derive(Debug)]
+pub struct CliEngine {
+    path: String,
+    cli_args: Option<String>,
+    time: Duration,
+    increment: Duration,
+    tei_settings: Vec<(String, String)>,
 }
 
 pub fn parse_cli_arguments() -> CliOptions {
@@ -44,13 +53,16 @@ pub fn parse_cli_arguments_from(
                 .default_value("5")
                 .value_parser(clap::value_parser!(u64).range(4..=8)),
         )
-        .arg(Arg::new("engine-path")
+        .arg(Arg::new("engine-flag")
             .help("Specify the file path of an engine. Must be used twice to add both engines.")
             .short('e')
             .long("engine")
-            .required(true)
-            .num_args(2)
-        )
+            .num_args(0..)
+            .action(ArgAction::Append))
+        .arg(Arg::new("engine-flag-all")
+            .help("Specify the file path of an engine. Must be used twice to add both engines.")
+            .long("all-engines")
+            .num_args(0..))
         .arg(Arg::new("concurrency")
             .help("Number of games to run in parallel.")
             .default_value("1")
@@ -94,23 +106,6 @@ pub fn parse_cli_arguments_from(
             .help("Shuffle the provided opening book.")
             .num_args(0)
             .requires("book"))
-        .arg(Arg::new("tc")
-            .help("Time control for each game, in seconds. Format is time+increment, where the increment is optional.")
-            .long("tc")
-            .num_args(1)
-            .required(true))
-        .arg(Arg::new("engine1-args")
-            .help("Command-line argument string to pass to engine 1.")
-            .long("engine1-args")
-            .value_name("args")
-            .allow_hyphen_values(true)
-        )
-        .arg(Arg::new("engine2-args")
-            .help("Command-line argument string to pass to engine 2.")
-            .long("engine2-args")
-            .value_name("args")
-            .allow_hyphen_values(true)
-        )
         .arg(Arg::new("log")
             .short('l')
             .long("log")
@@ -130,13 +125,104 @@ pub fn parse_cli_arguments_from(
         )
         .try_get_matches_from(itr)?;
 
-    let (time, increment) = parser::parse_tc(matches.get_one::<String>("tc").unwrap())
-        .unwrap_or_else(|err| panic!("{}", err));
-
-    let engine_path_matches = matches
-        .get_many::<String>("engine-path")
+    let engines: Vec<CliEngine> = matches
+        .get_occurrences::<String>("engine-flag")
         .unwrap()
-        .collect::<Vec<_>>();
+        .enumerate()
+        .map(|(id, engine)| {
+            let mut engine_path = None;
+            let mut engine_arg = None;
+            let mut engine_tc_str = None;
+            let mut tei_settings = vec![];
+
+            for arg in engine.chain(
+                matches
+                    .get_many::<String>("engine-flag-all")
+                    .into_iter()
+                    .flatten(),
+            ) {
+                if let Some((arg, value)) = arg.split_once('=') {
+                    if let Some(option_arg) = arg.strip_prefix("option.") {
+                        if tei_settings.iter().any(|(a, _)| a == option_arg) {
+                            panic!(
+                                "Duplicate value for tei argument {} for engine #{}",
+                                option_arg,
+                                id + 1
+                            )
+                        } else {
+                            assert_ne!(option_arg, "komi");
+                            tei_settings.push((option_arg.to_string(), value.to_string()));
+                        }
+                        eprintln!(
+                            "Warning: Parsed TEI option \"{}\" with value \"{}\", but it will be ignored",
+                            option_arg, value
+                        )
+                    } else {
+                        match arg {
+                            "path" if engine_path.is_some() => {
+                                panic!(
+                                    "Duplicate path arguments \"{}\" and \"{}\" for engine #{}",
+                                    engine_path.unwrap(),
+                                    value,
+                                    id + 1
+                                )
+                            }
+                            "path" => engine_path = Some(value),
+                            "arg" if engine_arg.is_some() => {
+                                panic!(
+                                    "Duplicate arg arguments \"{}\" and \"{}\" for engine #{}",
+                                    engine_arg.unwrap(),
+                                    value,
+                                    id + 1
+                                )
+                            }
+                            "arg" => engine_arg = Some(value),
+                            "tc" if engine_tc_str.is_some() => {
+                                panic!(
+                                    "Duplicate tc arguments \"{}\" and \"{}\" for engine #{}",
+                                    engine_tc_str.unwrap(),
+                                    value,
+                                    id + 1
+                                )
+                            }
+                            "tc" => engine_tc_str = Some(value),
+                            _ => panic!("Unknown argument {} for engine #{}", arg, id + 1),
+                        }
+                    }
+                } else {
+                    panic!("Invalid input {}", arg); // TODO: Don't panic, better error message
+                }
+            }
+
+            let (time, increment) = parser::parse_tc(engine_tc_str.unwrap()).unwrap(); // TODO: Error message
+
+            CliEngine {
+                path: engine_path.unwrap().to_string(), // TODO: Error message
+                cli_args: engine_arg.map(ToString::to_string),
+                time,
+                increment,
+                tei_settings,
+            }
+        })
+        .collect();
+
+    println!("Engines: ");
+    for engine in engines.iter() {
+        println!("{:?}", engine)
+    }
+    println!();
+
+    assert_eq!(
+        engines.len(),
+        2,
+        "Got {} engines, only 2 is supported",
+        engines.len()
+    );
+
+    assert!(engines.iter().all(|engine| engine.time == engines[0].time)); // TODO: Error message
+    assert!(engines
+        .iter()
+        .all(|engine| engine.increment == engines[0].increment)); // TODO: Error message
 
     let book_format = match matches.get_one::<String>("book-format").unwrap().as_str() {
         "move-list" => BookFormat::MoveList,
@@ -149,16 +235,10 @@ pub fn parse_cli_arguments_from(
         size: *matches.get_one::<u64>("size").unwrap() as usize,
         concurrency: *matches.get_one::<u64>("concurrency").unwrap() as usize,
         games: *matches.get_one::<usize>("games").unwrap(),
-        time,
-        increment,
-        engine_paths: [
-            engine_path_matches[0].to_string(),
-            engine_path_matches[1].to_string(),
-        ],
-        engine_args: [
-            matches.get_one("engine1-args").cloned(),
-            matches.get_one("engine2-args").cloned(),
-        ],
+        time: engines[0].time,           // TODO: Support asymmetric TC
+        increment: engines[0].increment, // TODO: Support asymmetric TC
+        engine_paths: [engines[0].path.to_string(), engines[1].path.to_string()],
+        engine_args: [engines[0].cli_args.clone(), engines[1].cli_args.clone()],
         pgnout: matches.get_one("file").cloned(),
         book_path: matches.get_one("book").cloned(),
         book_format,
